3905e48d8557a3336c0476b57bb211c0e0b7389d
masha-m12 <mary.sun.light.too@gmail.com>
2025-05-05 11:27:14 +0700

    сделаны remove, rename, в sortFile, убрано дублирование логики (которое есть в mergeFiles), splitFiles, mergeFiles теперь bool

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -56,59 +56,46 @@ bool isFileContainsSortedArray(const std::string& fileName) {
         }
         previous = current;
     }
+
     file.close();
     return true;
-
 }
 
-void splitFiles(const std::string& file, const std::string& fileA, const std::string& fileB) {
+bool splitFiles(const std::string& file, const std::string& fileA, const std::string& fileB) {
     std::ifstream f(file);
     std::ofstream fA(fileA);
     std::ofstream fB(fileB);
 
     if (!f.is_open() || !fA.is_open() || !fB.is_open()) {
         std::cerr << "Ошибка при открытии файлов." << "\n";
-        return;
+        return false;
     }
 
     int number1, number2;
-    bool inFile1 = true;
 
     if (!(f >> number1)) {
         std::cerr << "Файл пустой." << "\n";
-        return;
+        return false;
     }
 
     while (f >> number2) {
-        if (inFile1) {
-            fA << number1 << " ";
-        }
-        else {
-            fB << number1 << " ";
-        }
-
-        if (number1 <= number2) {
-            number1 = number2;
-        }
-        else {
-            number1 = number2;
-            inFile1 = !inFile1;
-        }
-    }
-
-    if (inFile1) {
         fA << number1 << " ";
+        if (number1 > number2) {
+            std::swap(fA, fB);
+        }
+        number1 = number2;
     }
-    else {
-        fB << number1 << " ";
-    }
+
+    fA << number1 << " ";
 
     f.close();
     fA.close();
     fB.close();
+
+    return true;
 }
 
-void mergeFiles(const std::string& fileA, const std::string& fileB, const std::string& fileC, const std::string& fileD) {
+bool mergeFiles(const std::string& fileA, const std::string& fileB, const std::string& fileC, const std::string& fileD) {
     std::ifstream fA(fileA);
     std::ifstream fB(fileB);
     std::ofstream fC(fileC);
@@ -116,7 +103,7 @@ void mergeFiles(const std::string& fileA, const std::string& fileB, const std::s
 
     if (!fA.is_open() || !fB.is_open() || !fC.is_open() || !fD.is_open()) {
         std::cerr << "Ошибка при открытии файлов." << "\n";
-        return;
+        return false;
     }
 
     Sequence seq1, seq2;
@@ -125,6 +112,8 @@ void mergeFiles(const std::string& fileA, const std::string& fileB, const std::s
 
     seq1.readNext();
     seq2.readNext();
+
+    bool flag = false;
 
     while (seq1.isValueValid || seq2.isValueValid) {
         while (!seq1.ended || !seq2.ended) {
@@ -141,6 +130,8 @@ void mergeFiles(const std::string& fileA, const std::string& fileB, const std::s
         seq1.ended = !seq1.isValueValid;
         seq2.ended = !seq2.isValueValid;
 
+        flag = flag || (seq1.isValueValid || seq2.isValueValid);
+
         fC.swap(fD);
     }
 
@@ -148,6 +139,8 @@ void mergeFiles(const std::string& fileA, const std::string& fileB, const std::s
     fB.close();
     fC.close();
     fD.close();
+
+    return flag;
 }
 
 std::string sortFile(const std::string& file,
@@ -163,23 +156,15 @@ std::string sortFile(const std::string& file,
     std::string output1 = fileC;
     std::string output2 = fileD;
 
-    while (true) {
-        mergeFiles(input1, input2, output1, output2);
-
-        Sequence seq;
-        std::ifstream currentInput(input2);
-        seq.file = &currentInput;
-        seq.readNext();
-
-        if (!seq.isValueValid) {
-            return output1;
-        }
-
+    while (mergeFiles(input1, input2, output1, output2)) {
         std::swap(input1, output1);
         std::swap(input2, output2);
     }
 
-    return output1;
+    std::remove(file.c_str());
+    std::rename(output1.c_str(), file.c_str());
+
+    return file;
 }
 
 int main() {
