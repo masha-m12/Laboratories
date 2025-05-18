4de63478559971aa955fcb16cc61c55c81415241
masha-m12 <mary.sun.light.too@gmail.com>
2025-05-18 23:29:54 +0700

    реализована еще часть методов, взята часть кода с лекций

--- lab2/lab2/BinaryTree.cpp
+++ lab2/lab2/BinaryTree.cpp
@@ -39,3 +39,96 @@ BinaryTree::BinaryTree(const BinaryTree& other) {
 BinaryTree::~BinaryTree() {
     clear(m_root);
 }
+
+BinaryTree::Node* BinaryTree::root() const {
+    return m_root;
+}
+
+void BinaryTree::clear() {
+    clear(m_root);
+    m_root = nullptr;
+}
+
+bool BinaryTree::isEmpty() const {
+    return m_root == nullptr;
+}
+
+int BinaryTree::countNodes() const {
+    return countNodes(m_root);
+}
+
+BinaryTree::Node* BinaryTree::addNode(Node* root, int key)
+{
+    if (!root) {
+        root = new Node(key);
+    }
+    else if (rand() % 2) {
+        root->setLeftChild(addNode(root->leftChild(), key));
+    }
+    else {
+        root->setRightChild(addNode(root->rightChild(), key));
+    }
+    return root;
+}
+
+bool BinaryTree::searchNodeByKey(int key) const {
+    return nlrSearch(m_root, key);
+}
+
+BinaryTree::Node* BinaryTree::copyTree(Node* root) {
+    if (!root) {
+        return nullptr;
+    }
+
+}
+
+void BinaryTree::clear(Node* node) {
+    if (!node) {
+        return;
+    }
+    clear(node->leftChild());
+    clear(node->rightChild());
+    delete node;
+    node = nullptr;
+}
+
+int BinaryTree::countNodes(Node* node) const {
+    if (!node) {
+        return 0;
+    }
+    return 1 + countNodes(node->leftChild()) + countNodes(node->rightChild());
+}
+
+void BinaryTree::printHorizontal(Node* root, int marginLeft, int levelSpacing) const
+{
+    if (root == nullptr) {
+        return;
+    }
+    printHorizontal(root->rightChild(), marginLeft + levelSpacing, levelSpacing);
+    std::cout << std::string(marginLeft, ' ') << root->key() << std::endl;
+    printHorizontal(root->leftChild(), marginLeft + levelSpacing, levelSpacing);
+}
+
+void BinaryTree::printLevels(Node* root, int leftBorderPos, int rightBorderPos, int yPos) const //реализация с презентации - доработать!
+{
+    if (root == nullptr) {
+        return;
+    }
+    int xPos = (leftBorderPos + rightBorderPos) / 2;
+    //ToDo: перенос курсор в точку (xPos, yPos)
+    //ToDo: вывод root->key() 
+    printLevels(root->leftChild(), leftBorderPos, xPos, yPos + 15);
+    printLevels(root->rightChild(), xPos, rightBorderPos, yPos + 15);
+}
+
+BinaryTree::Node* BinaryTree::nlrSearch(Node* root, int key) const
+{
+    if (!root || root->key() == key) {
+        return root;
+    }
+    Node* subTreeSearchResult = nlrSearch(root->leftChild(), key);
+    if (!subTreeSearchResult) {
+        subTreeSearchResult = nlrSearch(root->rightChild(), key);
+    }
+    return subTreeSearchResult;
+}
