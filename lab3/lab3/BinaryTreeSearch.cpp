fb3fddc3a4b535303073102df0a2f80d2e5f2140
masha-m12 <mary.sun.light.too@gmail.com>
2025-06-01 23:37:37 +0700

    доделана реализация методов, взята часть кода с лекций

--- lab3/lab3/BinaryTreeSearch.cpp
+++ lab3/lab3/BinaryTreeSearch.cpp
@@ -32,3 +32,99 @@ int BinaryTreeSearch::max(Node* node) const {
     }
     return current->key();
 }
+
+BinaryTreeSearch::Node* BinaryTreeSearch::addNode(Node* root, int key)
+{
+    if (!root) {
+        root = new Node(key);
+    }
+    else if (key < root->key()) {
+        root->setLeftChild(addNode(root->leftChild(), key));
+    }
+    else {
+        root->setRightChild(addNode(root->rightChild(), key));
+    }
+
+    return root;
+}
+
+BinaryTreeSearch::Node* BinaryTreeSearch::remove(Node* node, int key) {
+    if (!node) {
+        return nullptr;
+    }
+
+    Node* replacement = nullptr;
+
+    if (node->key() > key) {
+        Node* newLeftChild = remove(node->leftChild(), key);
+        node->setLeftChild(newLeftChild);
+    }
+    else if (node->key() < key) {
+        Node* newRightChild = remove(node->rightChild(), key);
+        node->setRightChild(newRightChild);
+    }
+    else {
+        if (!node->leftChild() && !node->rightChild()) {
+            replacement = nullptr;
+        }
+        else if (!node->leftChild()) {
+            replacement = node->rightChild();
+        }
+        else {
+            replacement = node->leftChild();
+        }
+
+        Node* replacement = node->rightChild();
+        while (replacement->leftChild()) {
+            replacement = replacement->leftChild();
+        }
+
+        node->setKey(replacement->key());
+        Node* newRightChild = remove(node->rightChild(), replacement->key());
+        node->setRightChild(newRightChild);
+    }
+    return node;
+}
+
+BinaryTreeSearch::Node* BinaryTreeSearch::nlrSearch(Node* node, int key) const {
+    if (node == nullptr) {
+        return nullptr;
+    }
+
+    if (node->key() == key) {
+        return node;
+    }
+    else if (node->key() > key) {
+        return nlrSearch(node->leftChild(), key);
+    }
+    else {
+        return nlrSearch(node->rightChild(), key);
+    }
+}
+
+int BinaryTreeSearch::getLevelByKey(Node* node, int key, int level) const {
+    if (!node) {
+        return -1;
+    }
+
+    if (node->key() == key) {
+        return level;
+    }
+    else if (key < node->key()) {
+        return getLevelByKey(node->leftChild(), key, level + 1);
+    }
+    else {
+        return getLevelByKey(node->rightChild(), key, level + 1);
+    }
+}
+
+void BinaryTreeSearch::lrnTraversal(Node* root, std::vector<int>& keys) const
+{
+    if (root == nullptr) {
+        return;
+    }
+
+    lrnTraversal(root->leftChild(), keys);
+    keys.push_back(root->key());
+    lrnTraversal(root->rightChild(), keys);
+}
--- lab3/lab3/BinaryTreeSearch.h
+++ lab3/lab3/BinaryTreeSearch.h
@@ -11,6 +11,14 @@ public:
     BinaryTreeSearch(BinaryTreeSearch&& other) noexcept;
     ~BinaryTreeSearch() override = default;
 
+    using BinaryTree::min;
+    using BinaryTree::max;
+    using BinaryTree::addNode;
+    using BinaryTree::deleteNodeByKey;
+    using BinaryTree::searchNodeByKey;
+    using BinaryTree::getLevelByKey;
+    using BinaryTree::allTreeKeys;
+
     BinaryTreeSearch& operator=(const BinaryTreeSearch& other);
     BinaryTreeSearch& operator=(BinaryTreeSearch&& other) noexcept;
 
@@ -19,7 +27,7 @@ protected:
     int max(Node* node) const override;
 
     Node* addNode(Node* root, int key) override;
-    Node* remove(Node* node, Node* parent) override;
+    Node* remove(Node* node, int key);
     Node* nlrSearch(Node* node, int key) const override;
 
     int getLevelByKey(Node* node, int key, int level) const override;
