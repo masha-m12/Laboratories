754b8a0d8dcdde46e4110d9807acb2c3cccb7b0a
masha-m12 <mary.sun.light.too@gmail.com>
2025-06-11 21:39:24 +0700

    реализованы методы класса Node, переписан buid

---
+++ lab5/lab5/HuffmanTree.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <array>
+#include <string>
+#include <iostream>
+#include "C:/Users/Mariya/Desktop/Gittyup/OOP/BoolVector/BoolVector/BoolVector.h"
+
+class HuffmanTree
+{
+    class Node
+    {
+    public:
+        Node();
+        ~Node();
+
+        std::string stringifySymbols() const;
+
+        void printHorizontal(Node* root, int marginLeft = 0, int levelSpacing = 4) const;
+
+        int frequency() const;
+        void setFrequency(int frequency);
+
+        const BoolVector& symbols() const;
+        void setSymbols(const BoolVector& symbols);
+
+        Node* left() const;
+        void setLeft(Node* left);
+
+        Node* right() const;
+        void setRight(Node* right);
+
+    private:
+        int m_frequency;
+        BoolVector m_symbols = BoolVector(256, 0);
+
+        Node* m_left = nullptr;
+        Node* m_right = nullptr;
+    };
+
+public:
+    HuffmanTree() = default;
+    ~HuffmanTree();
+
+    void clear();
+
+    void print();
+
+    void build(const std::string& text);
+    double encode(const std::string& text, std::string& encodedText);
+    bool decode(const std::string& encodedText, std::string& decodedText);
+
+private:
+    std::string encode(const char symbol);
+    bool decode(std::istream& encodedStream, std::ostream& decodedStream);
+
+private:
+    Node* m_root = nullptr;
+};
---
+++ lab5/lab5/lab5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <numeric>
+
+#include "HuffmanTree.h"
+
+int main()
+{
+    HuffmanTree tree;
+    tree.build("ehal greka cerez reku");
+    tree.print();
+    return 0;
+}
+
+BoolVector::BoolVector(int length)
+    : m_length(length) {
+    m_cellCount = m_length / CellSize;
+    if (m_length % CellSize != 0) {
+        m_cellCount++;
+    }
+    m_cells = new Cell[m_cellCount];
+}
